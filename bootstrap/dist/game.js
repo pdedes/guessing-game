214f6cd20036c71357308fa20c2c24f85209f6cb
pdedes <dedes1821@gmail.com>
2015-01-09 12:49:06 -0500

    Added comments.

--- bootstrap/dist/game.js
+++ bootstrap/dist/game.js
@@ -1,10 +1,10 @@
 var gameProto = {
-    guess: 0,
-    target: 0, //store number
-    numTries: 0,
+    guess: 0, //Last guess by player
+    target: 0, //store game target to be guessed
+    numTries: 0, //Guess counter
     pastGuesses: [], //Store all previous attempts
 
-    
+    //Main game function evaluates player's guess.
     checkGuess: function (num) {
         this.guess = num;
         while (this.guess < 1 || this.guess > 100) {
@@ -21,10 +21,12 @@ var gameProto = {
     },
     
     notify: function () {
+        //Calculate distance & determine positivity, then determine absolute distance
         var distance = this.target - this.guess;
         var intPositive = distance > 0 ? true : false;
         distance = Math.abs(distance);
 
+        //This determines distance from the target, and informs player to guess in the correct direction (pos or neg)
         if (distance < 10) {
             if (intPositive) {console.log("You're very hot! Guess higher.");} 
             else {console.log("You're very hot! Guess lower.");}
@@ -38,11 +40,14 @@ var gameProto = {
     },
 }
 
+//Setting 'this' for the Game prototype and initialized values.
 function Game () {
     this.target = Math.floor(Math.random()*100)+1;
     this.numTries = 0;
 }
 
+//Assigning inheritance.
 Game.prototype = gameProto;
 
+//Declaring a new game.
 newGame = new Game("newGame");
